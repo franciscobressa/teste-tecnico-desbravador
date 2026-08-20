70c94b2d3b6012ceb55373510069d644ef911df6
Francisco Bressa <bressafrancisco@gmail.com>
2026-08-19 22:33:50 -0400

    inicializando projetto com vite

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const app = document.querySelector('#app')
+
+app.innerHTML = `
+  <section class="home-placeholder">
+    <h1>GitHub Explorer</h1>
+    <p>Busque repositórios populares de um usuário do GitHub.</p>
+  </section>
+`
