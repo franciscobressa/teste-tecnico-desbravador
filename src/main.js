6f19c06f5dc8272ce68b52baba310badcc0dd4fc
Francisco Bressa <bressafrancisco@gmail.com>
2026-08-19 22:36:56 -0400

    bootstrap e axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './styles/main.css'
+
 const app = document.querySelector('#app')
 
 app.innerHTML = `
   <section class="home-placeholder">
-    <h1>GitHub Explorer</h1>
-    <p>Busque repositórios populares de um usuário do GitHub.</p>
+    <h1 class="display-5 fw-bold mb-3">GitHub Explorer</h1>
+    <p class="lead text-muted">
+      Busque repositórios populares de um usuário do GitHub.
+    </p>
   </section>
 `
